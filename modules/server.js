c5d3b96c46699745f4003adc269bfbbd77fc93c6
João Pedro Marques <joaopedrobmb@gmail.com>
2026-01-20 19:10:06 +0000

    add `/top-countries` endpoint.

--- modules/server.js
+++ modules/server.js
@@ -3,32 +3,55 @@ const http = require("http");
 const users = [];
 
 const server = http.createServer((req, res) => {
+  const start = Date.now();
   // POST /users
   if (req.method === "POST" && req.url === "/users") {
     let body = "";
 
     req.on("data", (chunk) => {
       body += chunk;
+    });
+    req.on("end", () => {
+      const newUsers = JSON.parse(body);
+      users.push(...newUsers);
 
-      req.on("end", () => {
-        const newUsers = JSON.parse(body);
-        users.push(...newUsers);
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Usuários adicionados!" }));
-      });
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Usuários adicionados!" }));
     });
   }
   // GET /superusers
-  if (req.method === "GET" && req.url === "/superusers") {
-    res.on("end", () => {
-      const filteredUsers = users.filter(
-        (user) => user.score >= 900 && user.active === true,
-      );
+  else if (req.method === "GET" && req.url === "/superusers") {
+    const superUsers = users.filter(
+      (user) => user.score >= 900 && user.active === true,
+    );
 
-      res.writeHead(200, { Content_Type: "application/json" });
-      res.end(JSON.stringify({ filtered_users: filteredUsers }));
-    });
-  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        time_spent: Date.now() - start,
+        filtered_users: superUsers,
+      }),
+    );
+  }
+  // GET /top-countries
+  else if (req.method === "GET" && req.url === "/top-countries") {
+    const superUsers = users.filter(
+      (user) => user.score >= 900 && user.active === true,
+    );
+
+    const countPerCountry = superUsers.reduce((acc, user) => {
+      acc[user.country] = (acc[user.country] || 0) + 1;
+      return acc;
+    }, {});
+
+    const sortedCountPerCountry = Object.entries(countPerCountry);
+    sortedCountPerCountry.sort((a, b) => b[1] - a[1]);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(countPerCountry));
+  }
+  //
+  else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
   }
@@ -41,7 +64,7 @@ server.listen(3000, "localhost", () => {
 // POST /users OK
 // Recebe e armazena os usuários na memória. Pode simular um banco de dados em memória.
 
-// GET /superusers
+// GET /superusers OK
 // Filtro: score >= 900 e active = true
 // Retorna os dados e o tempo de processamento da requisição.
 
